fcba45be445c64259b6ecd4b264fc5572121a313
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2026-02-08 16:28:49 -0800

    Update pairwiseplot.py
    Changed dimension of the plotting area

--- plots/pairwiseplot.py
+++ plots/pairwiseplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def show_pairwise_relations(pairs):
-    plt.figure(figsize=(10,4))
+    plt.figure(figsize=(6,3))
     sns.pairplot(pairs, vars=['coint_pvalue','norm_price_dist','spread_vol','half_life'])
     plt.suptitle("Pairwise Relationships Between Metrics", y=1.02)
     plt.show()
