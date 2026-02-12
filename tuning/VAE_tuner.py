6990aac535f477e0b52015b1adf3e622f43d1654
SantoshMore2020 <71905967+SantoshMore2020@users.noreply.github.com>
2026-02-11 21:03:57 -0800

    Update VAE_tuner.py
    Changed the number of trials from 10 to 30

--- tuning/VAE_tuner.py
+++ tuning/VAE_tuner.py
@@ -210,7 +210,7 @@ class VAE_tuner(hyperparameter_tuner):
 
         return float(np.mean(val_losses))
 
-    def tune(self, spreads, bocpd_cfg, n_trials=10): # 20
+    def tune(self, spreads, bocpd_cfg, n_trials=30): # 20
         best_loss = np.inf
         best_cfg = None
 
